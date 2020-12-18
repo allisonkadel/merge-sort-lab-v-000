cc6083f37f3293feaddab6c247dc4cef1ee0a875
Ally Kadel <akadel01@gmail.com>
2020-12-18 14:43:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function mergeSort(array){
   // divide the array into half until only 1-element arrays are left
   let subset1 = array.slice(0,array.length/2)
   let subset2 = array.slice(array.length/2)
+  let sorted;
   if (array.length < 2){
     return array;
   } else {
