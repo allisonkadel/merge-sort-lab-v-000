23f31db4c485d8f962fe514966403149fac4f0ff
Ally Kadel <akadel01@gmail.com>
2020-12-18 14:32:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function mergeSort(arrays){
+  
+}
 
+function findMinAndRemoveSorted(array){
+  return array.shift();
+}
+
+function merge(array1, array2){
+  let sorted = [];
+  while (array1.length > 0 && array2.length > 0){
+    if (array1[0] < array2[0]){
+      sorted.push(findMinAndRemoveSorted(array1));
+    }
+    else {
+      sorted.push(findMinAndRemoveSorted(array2));
+    }
+  }
+  return sorted.concat(array1, array2);
+}
